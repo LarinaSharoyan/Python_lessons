69eb620d72c8f317e4368efb458db808b1e29f7e
LarinaSharoyan <sharoyanlarina@gmail.com>
2023-08-25 04:01:22 +0400

    adding selected sort

--- Algorithms/selected_sort.py
+++ Algorithms/selected_sort.py
@@ -1,6 +1,11 @@
-lst = [5, 3, 5, 6, 0,-9]
-lst2 = []
-while len(lst) != 0:
-    lst2.append(max(lst))
-    lst.remove(max(lst))
-print(lst2)
+"""Sorting algorithms/ selected sort"""
+def selected_sort(lst):
+    """This functions is doing a selected sort"""
+    lst2 = []
+    while len(lst) != 0:
+        lst2.append(max(lst))
+        lst.remove(max(lst))
+    return lst2
+list1 = [5, 3, 5, 6, 0,-9]
+sorted_arr = selected_sort(list1)
+print(sorted_arr)
