b387950eda8a08e78ff876454c9cbfa4796214c3
LarinaSharoyan <sharoyanlarina@gmail.com>
2023-08-29 01:22:29 +0400

    add questions

--- millionaire/add_questions.py
+++ millionaire/add_questions.py
@@ -1,7 +1,13 @@
+"""This program is part of millionaire for operator,
+it asks about to add new questions and options"""
+import sys
+
 def ask():
+    """Ask if user wants to add new questions"""
     add = input('Would you like to add questions?: ')
     return add.lower() == "yes" or add.lower() == "y"
 def add_line():
+    """asks for question, options, correct answer and returns"""
     qst = input('enter your question: ')
     while qst[-1] != '?':
         print("In the end of the question please add ? sign")
@@ -13,26 +19,34 @@ def add_line():
         options = input("""Please Enter your options like this
 ("correct,wrong,wrong,wrong"): """)
     return qst+options.strip()
-def return_lines(file):
-    with open(file) as f:
-        return f.readlines()
-def check_qst_existence(lst, line):
-    for i in lst:
-        if line.split('?')[0] == i.split('?')[0]:
-            return False
-        return True
-def file_append(file, line):
+def return_lines(filename):
+    """returns all questions that exist"""
     try:
-        with open(file, 'a') as fl:
+        with open(filename, encode='utf-8') as file:
+            return file.readlines()
+    except FileExistsError:
+        print("There's no such a file. Please check")
+        sys.exit()
+def check_qst_existence(lst, line):
+    """check if given question exists or not"""
+    for quest in lst:
+        if line.split('?')[0] == quest.split('?')[0]:
+            return False
+    return True
+def file_append(file, line):
+    """writes the new question in the file"""
+    try:
+        with open(file, 'a', encode='utf-8') as fl:
             fl.write(line+'\n')
     except:
         print("Your file does not exist. Please check")
-        exit()
+        sys.exit()
 def main():
+    """asks for adding and does appropriate functions"""
     if ask():
         mstr = add_line()
-        if add_line():
+        if check_qst_existence(return_lines("qst.txt"), mstr):
             file_append('qst.txt', mstr)
+        else:
+            print('sorry, same question already exists')
 main()
-            
-
